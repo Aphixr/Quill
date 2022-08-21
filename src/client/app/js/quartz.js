8c7e1be267a104f4a020c7acfeb1fa33bc75929b
Supernova12888 <supernova12888@gmail.com>
2022-08-21 16:56:23 -0400

    Changed to Delta

--- src/client/app/js/quartz.js
+++ src/client/app/js/quartz.js
@@ -1,225 +1,67 @@
-/** Quartz.js - Gamma 1.0.0
+/**
+ * Quartz.js Delta
  * 
- * hope I don't have to go back to this file to fix anything
- * or even worse, start all over
+ * A much simpler version, without states and actions.
+ * Extend from `Component` class to create a component
  * 
- * note to self, example usage:
- *
+ * Example usage:
     class Counter extends Component {
         constructor() {
-            super();
-            this.state.count = 0;
-            this.action.incre = (n) => this.state.count += n;
+            super(document.createElement('button'));
+            this.count = 0;
+            this.element.addEventListener(() => {
+                return this.element.innerText = ++this.count;
+            });
         }
-        render() {
-            return html`
-                <button @click="${this.action.incre(Math.random())}">
-                    counter: ${this.state.count}
-                </button>
-            `;
+        increase() {
+            return ++this.count;
         }
     }
     const counter = new Counter();
-    Component.render(editor.content, counter);
-    counter.action.incre()(1); // < using actions outside
- *
+    Component.render(document.body, counter);
  */
-
 
 
 "use strict";
 
-// HTML content class
-class HTMLContent {
-    // Constructor
-    constructor(content, actions) {
-        this.element = document.createElement("div");
-        this.content = content;
-        this.actions = actions;
-
-        // Add things to the element
-        this.element.innerHTML = this.content;
-        this.element.classList.add("q-html-content");
-    }
-
-    // Initialize event listeners
-    initEventListeners(thisBind) {
-        // Loop through all children
-        for (let i = 0, len = this.element.children.length; i < len; i++) {
-            const child = this.element.children[i];
-
-            // Get all attributes that start with '@'
-            const ats = child.getAttributeNames().filter((name) => name.charAt(0) === "@");
-            
-            // Add event listeners
-            for (let j = 0, len = ats.length, actIdx = 0; j < len; j++, actIdx++) {
-                child.addEventListener(ats[j].substring(1), (evt) => {
-                    if (this.actions[actIdx] && this.actions[actIdx].toString() == child.getAttribute(ats[j])) {
-                        return this.actions[actIdx].call(thisBind, ...this.actions[j].args);
-                    } else {
-                        actIdx--;
-                        return new Function(`const event = ${JSON.stringify(evt)};${child.getAttribute(ats[j])}`)();
-                    }
-                });
-            }
-        }
-    }
-}
-
-// HTML tag function
-// Returns an instance of `HTMLContent`
-const html = (strings, ...expressions) => {
-    // Piece the strings and expressions array together
-    let content = "";
-    const actions = [];
-    for (let i = 0, len = expressions.length; i < len; i++) {
-        // Put the string
-        content += strings[i];
-
-        // Put the expressions
-        content += (() => {
-            // Check an expression is a component state or action
-            if (expressions[i] instanceof ComponentState) {
-                return `<q-state name='${expressions[i].name}'>${String(expressions[i])}</q-state>`;
-            }
-            if (expressions[i] && expressions[i].isAction) {
-                actions.push(expressions[i]);
-                return `${String(expressions[i])}`
-                    .replace(/(?<!\\)(")/g, "`")
-                    .replace(/(?<!\\)(')/g, "`");
-            }
-
-            return expressions[i];
-        })();
-    }
-    // Add last string
-    content += strings[strings.length - 1];
-
-    // Return HTMLContent
-    return new HTMLContent(content.trim(), actions);
-};
-
-// State class
-class ComponentState {
-    constructor(owner, name, value) {
-        this.owner = owner;
-        this.name = String(name);
-        this.value = value;
-
-        // Symbol.toPrimitive can convert an object into a number or string
-        this[Symbol.toPrimitive] = function(type) {
-            switch (type) {
-                case "number":
-                    return Number(this.value);
-                case "string":
-                    return String(this.value);
-                case "default":
-                default:
-                    return this.value;
-            }
-        }
-    }
-}
-
-// Action class
-class ComponentAction {
-    constructor(owner, name, func) {
-        this.owner = owner;
-        this.name = String(name);
-
-        if (typeof func !== "function") {
-            throw new TypeError("ComponentAction ctor arg 3 is not a function");
-        }
-        this.func = func;
-        
-        // Attach some properties to the function
-        // that will be used else where
-        // such as the `html` tag function
-        this.func.isAction = true;
-        this.func.owner = this.owner;
-        const fn = this.func;
-        this.costume = function() {
-            fn.args = arguments;
-            return fn;
-        };
-    }
-}
-
 // Component class
-// Inherit from this class to create a component
 class Component {
     // Constructor
-    constructor() {
-        this.element = document.createElement("div");
-        this.where = null;
-        this.id = (Math.random() * new Date).toString(36).substring(0, 6);
-        this.element.classList.add(`q-ci-${this.id}`);
-        this.element.classList.add("q-component");
-        
-        // State and action proxies
-        const stateTarget = {}, actionTarget = {};
-        this.state = new Proxy(stateTarget, {
-            get: (_, property) => {
-                return stateTarget[property];
-            },
-            set: (_, property, value) => {
-                // If the property already exists,
-                // just change the value and update component
-                if (property in stateTarget) {
-                    stateTarget[property].value = value;
-
-                    const placesUsed = this.element.querySelectorAll(`q-state[name="${property}"]`);
-                    for (let i = 0, len = placesUsed.length; i < len; i++) {
-                        placesUsed[i].innerText = value;
-                    }
-                }
-                return stateTarget[property] = new ComponentState(this, property, value);
-            }
-        });
-        this.action = new Proxy(actionTarget, {
-            get: (_, property) => {
-                return actionTarget[property].costume;
-            },
-            set: (_, property, func) => {
-                // Check if it's a fun! (-ction)
-                if (typeof func !== "function") {
-                    throw new TypeError(`this.action['${property}'] rhs is not a function`);
-                }
-                
-                // If the property already exists, say no
-                if (property in actionTarget) {
-                    throw new TypeError(`this.action['${property}'] cannot be redefined`);
-                }
-                return actionTarget[property] = new ComponentAction(this, property, func);
-            }
-        });
+    constructor(element) {
+        this.element = element;
+        this.components = {};
     }
 
-    // Attach component(s)
-    static render(where, ...components) {
-        // where.innerHTML = "";
+    // Render the component
+    render(where) {
+        where.appendChild(this.element);
+        return this;
+    }
 
-        // Loop through all the components the user wants to attach
-        for (let i = 0, len = components.length; i < len; i++) {
-            const component = components[i];
-
-            // Check if the argument passed is actually a Component
-            if (component instanceof Component) {
-                component.where = where;
-
-                // Render
-                const render = component.render();
-                render.initEventListeners(component);
-                component.element.appendChild(render.element);
-                // console.log(components[i].element.content);
-
-                // Append component element
-                where.appendChild(component.element);
-            }
+    // Adds a component to this component
+    setComponent(name, component) {
+        if (!(component instanceof Component)) {
+            throw new TypeError("Unexpected value for argument 2");
         }
+        this.components[name] = component;
+        return this.components[name];
+    }
+
+    // Get a component
+    getComponent(name) {
+        return this.components[name];
+    }
+
+    // Attach a component onto this component's element
+    attachComponent(name) {
+        if (!(name in this.components)) {
+            throw new ReferenceError(`Cannot attach non-existing component '${name}'`);
+        }
+        this.element.appendChild(this.components[name].element);
+        return this.components[name];
     }
 }
 
-export { HTMLContent, html, ComponentAction, ComponentState, Component }
+export default Component
 
 
