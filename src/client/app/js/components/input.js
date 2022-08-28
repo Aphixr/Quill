b4f33f3777b24ccdccf261c6e63793cbffbd8f74
Supernova12888 <supernova12888@gmail.com>
2022-08-27 22:23:14 -0400

    Added Input derived and Navigator classes

--- src/client/app/js/components/input.js
+++ src/client/app/js/components/input.js
@@ -3,102 +3,178 @@
  * 
  * Anything to do with inputs, such as an input box,
  * radios, buttons, checkboxes, select, etc. `Input`
- * is an abstract class. `Tabs`, `Tab`, and `TabContent`
- * are also found here, extending from `Input` class.
+ * is an abstract class. `navigator` and related
+ * classes are also here.
  */
 
 
 "use strict";
 
 import dev from "../dev.js"
+import quill from "../quill.js"
 import Component from "../quartz.js"
 
 // Input class (abstract)
 // Anything the user can input any form of information
 // or interaction is extended from this class
+// REV: is this class too useful?
 class Input extends Component {
-    constructor() {
+    // Constructor
+    // `element` argument should be a `document.createElement()`
+    constructor(element) {
         dev.class.abstract(Input);
-        super(document.createElement("div"));
+        super(element);
 
         // Initialize element
         this.element.classList.add("input");
     }
-
-    // Set the container id
-    setContainerId(id) {
-        return this.element.id = String(id);
-    }
-
-    // Set the container classes
-    setContainerClasses(...classes) {
-        for (const className of classes) {
-            this.element.classList.add(className);
-        }
-        return this.element.classList;
-    }
-
-    // Create child element
-    createChildElement(name, {
-        content, attributes,
-        classes, id
-    }) {
-        // Create inner element
-        this.childElement = document.createElement(name);
-
-        // Set inner text
-        if (content) {
-            this.childElement.innerText = content;
-        }
-        
-        // Set any attributes
-        // Must be an object
-        if (dev.isType("object", attributes)) {
-            for (const attribute in attributes) {
-                this.childElement.setAttribute(attribute, attributes[attribute]);
-            }
-        }
-
-        // Set classes
-        // `classes` must be a string or an iterable
-        if (dev.isType("string", classes)) {
-            this.childElement.className = classes
-        } else if (dev.isIterable(classes)) {
-            for (const className of classes) {
-                this.childElement.classList.add(className);
-            }
-        } else;
-
-        // Set id
-        if (id) {
-            this.childElement.id = id;
-        }
-
-        // Append child element to container
-        this.element.appendChild(this.childElement);
-
-        return this.childElement;
-    }
-
-    // Add event listener to child element
-    addEventListener() {
-        this.childElement.addEventListener(...arguments);
-    }
-
-    // Remove event listener to child element
-    removeEventListener() {
-        this.childElement.removeEventListener(...arguments);
-    }
 }
+
+/* ===================== */
+/* General Input derived */
+/* ===================== */
 
 // Button class
 class Button extends Input {
     constructor(info) {
-        super();
-        this.createChildElement("button", info);
+        super(document.createElement("button"));
+        this.element.classList.add("button");
+        this.setProperties(info);
     }
 }
 
-export { Input, Button }
+// Toggler
+// A button that toggles something on or off
+class Toggler extends Button {
+    constructor(info, initialIsActive) {
+        super(info);
+        this.isActive = !!initialIsActive;
+        this.onActive = this.onInactive = null;
+        this.element.classList.add("toggler");
+    }
+    setOnclickListener() {
+        // Does addEventListener change `this`?
+        this.element.addEventListener("click", () => {
+            this.isActive = !this.isActive;
+            if (this.isActive) {
+                this.onActive();
+            } else {
+                this.onInactive();
+            }
+        });
+    }
+    setActiveListener(callback) {
+        this.onActive = callback;
+        if (this.onInactive) {
+            this.setOnclickListener();
+        }
+    }
+    setInactiveListener(callback) {
+        this.onInactive = callback;
+        if (this.onActive) {
+            this.setOnclickListener();
+        }
+    }
+}
+
+// Input field
+class TextField extends Input {
+    constructor(info) {
+        super(document.createElement("input"));
+        this.setProperties(info);
+    }
+}
+
+/* ===================== */
+/* Navigator classes    */
+/* ===================== */
+
+// Navigator button
+// A button in the menu the user can click on
+class NavigatorButton extends Button {
+    constructor(info) {
+        super(info);
+        this.element.classList.add("navigator-button");
+    }
+}
+
+// Navigator menu
+// Shows all the links/buttons
+class NavigatorMenu extends Component {
+    constructor() {
+        super(document.createElement("div"));
+        this.element.classList.add("navigator-menu");
+    }
+
+    // Add some buttons onto the navigator menu
+    addButtons(...navigatorButtons) {
+        for (let i = 0; i < navigatorButtons; i++) {
+            this.addComponent("button-" + i, navigatorButtons[i]);
+        }
+    }
+}
+
+// Navigator toggle button
+// Opens and closes the navigator
+class NavigatorToggler extends Toggler {
+    constructor(targetMenu, info) {
+        // Call parent constructor
+        const superArgument = {
+            // Use Quill logo for the menu
+            // REV: add image paths to `quill` object?
+            innerHTML: /* html */`
+                <img src="${quill.path.logo}">
+            `
+        };
+        Object.assign(superArgument, info);
+        super(superArgument);
+
+        // Target menu is the element that will be opened/closed
+        // when clicked on
+        // Target menu must be a component, not the element
+        if (!(targetMenu instanceof Component)) {
+            dev.throw({
+                Type: TypeError,
+                message: "Expected Component instance for argument 1"
+            });
+        }
+        this.targetMenu = targetMenu;
+
+        // Initialize element stuff
+        this.element.classList.add("navigator-toggler");
+
+        // Set on (in)active listeners
+        this.setActiveListener(() => {
+            this.targetMenu.element.classList.add("active");
+            this.targetMenu.element.classList.remove("inactive");
+        });
+        this.setInactiveListener(() => {
+            this.targetMenu.element.classList.add("inactive");
+            this.targetMenu.element.classList.remove("active");
+        });
+    }
+}
+
+// Navigator
+// Combines all the previous classes into one
+class Navigator {
+    constructor() {
+        // Properties
+        // The menu; all buttons will be display here
+        this.menu = new NavigatorMenu();
+        // The toggler button
+        this.toggler = new NavigatorToggler(this.menu);
+    }
+}
+
+// Export
+export {
+    // Input
+    Input,
+    Button, Toggler,
+    TextField,
+    // Navigator related
+    Navigator, NavigatorButton
+}
 
 
