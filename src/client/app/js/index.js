98d0829a7ffe6641aa7da7da330c1a2412a83783
Supernova12888 <supernova12888@gmail.com>
2022-08-28 22:22:51 -0400

    Resizable editor navigator

--- src/client/app/js/index.js
+++ src/client/app/js/index.js
@@ -8,31 +8,61 @@
 
 "use strict";
 
+import dev from "./dev.js"
 import quill from "./quill.js"
-import { Editor, Navigator, TextField } from "./components.js"
+import {
+    Editor, Navigator, TextField, HorizontalResizer
+} from "./components.js"
 
 
 /* =================== */
 /* Load                */
 /* =================== */
 
+// Components
 const navigator = new Navigator();
 const editor = new Editor();
+const editorPanelMenu = editor.getComponent("panel", "menu");
+const editorViewNavigation = editor.getComponent("view", "navigation");
+const editorViewContent = editor.getComponent("view", "content");
 
 // Attaches the navigator toggler to the menu bar
 // Toggler is an image of logo
-editor.getComponent("panel", "menu")
-    .addComponent("navigatorToggler", navigator.toggler);
+editorPanelMenu.addComponent("navigatorToggler", navigator.toggler);
 
 // Attach the input for changing the notebook title
-editor.getComponent("panel", "menu")
-    .addComponent("notebookTitleTextField", new TextField({
-        id: "notebook-title",
-        value: "Untitled notebook",
-        placeholder: "Notebook title",
-        title: "Notebook title"
-    }));
+editorPanelMenu.addComponent("notebookTitleTextField", new TextField({
+    id: "notebook-title",
+    value: "Untitled notebook",
+    placeholder: "Notebook title",
+    title: "Notebook title"
+}));
 
+// Add the horizontal resizer
+editorViewNavigation.addComponent("horizontalResizer", new HorizontalResizer("right"));
+
+// Add listeners
+editorViewNavigation.getComponent("horizontalResizer").setMousedownListener();
+editorViewNavigation.getComponent("horizontalResizer")
+    .setMouseupListener(function(event) {
+        // Collapse the editor navigation completely
+        // if the cursor reaches a certain point
+        if (event.clientX < 120) {
+            this.element.style.width = this.element.style.minWidth = "0px";
+        } else {
+            this.element.style.minWidth = "14rem";
+        }
+    });
+editorViewNavigation.getComponent("horizontalResizer")
+    .setMousemoveListener(function(event) {
+        // Update width
+        const nav = this.element;
+        nav.style.minWidth = "";
+        nav.style.width = `${event.clientX}px`;
+        editorViewContent.element.style.width = `${dev.getPageSize().width - event.clientX}px`;
+    });
+
+// Loading milestone (3/4)
 quill.milestoneTrack.done("loading");
 
 
@@ -42,6 +72,7 @@ quill.milestoneTrack.done("loading");
 
 editor.render(quill.app);
 
+// Display milestone (4/4)
 quill.milestoneTrack.done("display");
 
 
