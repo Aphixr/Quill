c5971fbcf1d1cbb23398db4d18652424bad265fa
Supernova12888 <supernova12888@gmail.com>
2022-09-10 17:54:30 -0400

    Moved editor code into one file

--- src/client/app/js/components/editor.js
+++ src/client/app/js/components/editor.js
@@ -1,17 +1,18 @@
 /**
- * js/editor/editor-panel.js
+ * js/editor/editor.js
  * 
- * Controls for editing notebooks. Menu tabs and tab
- * content are found here. Does not include notebook
- * navigation (found in editor-view.js).
+ * The main part of the app. Includes the editor panel on
+ * the top, with controls and menus, and the editor content,
+ * which displays the notebook sections and pages
  */
 
 
 "use strict";
 
-import Component from "../../quartz.js"
-import quill from "../../quill.js"
-import { Navigator, NavigatorButton, Button, TextField } from "../input.js"
+// Import
+import Component from "../quartz.js"
+import quill from "../quill.js"
+import { Navigator, NavigatorButton, Button, TextField } from "./input.js"
 
 // Editor menu bar
 // Has tabs that you can click on and edit notebook title
@@ -183,8 +184,72 @@ class EditorPanel extends Component {
     }
 }
 
+// Editor sidebar
+// Displays content of whatever tab user clicked on in EditorActivity
+class EditorSideBar extends Component {
+    constructor() {
+        super(document.createElement("div"));
+        this.element.id = "editor-side-bar";
+    }
+}
+
+// Editor activity bar
+// Shows tabs that can be switched between
+class EditorActivityBar extends Component {
+    constructor() {
+        super(document.createElement("div"));
+        this.element.id = "editor-activity-bar";
+    }
+}
+
+// Editor content
+// Displays the content of a notebook page
+class EditorContent extends Component {
+    constructor() {
+        super(document.createElement("div"));
+        this.element.id = "editor-content";
+    }
+}
+
+// Editor view
+// Shows the contents of the notebook and notebook page
+class EditorView extends Component {
+    constructor() {
+        // Initialize this element
+        super(document.createElement("div"));
+        this.element.id = "editor-view";
+        this.element.classList.add("flex");
+
+        // Add components
+        this.activityBar = this.addComponent("activity-bar", new EditorActivityBar);
+        this.sideBar = this.addComponent("side-bar", new EditorSideBar);
+        this.content = this.addComponent("content", new EditorContent);
+        
+        // Initialize the navigator
+        this.activityNavigator = new Navigator();
+    }
+}
+
+// Editor
+// The main part of the program is here
+class Editor extends Component {
+    constructor() {
+        // Initialize element
+        super(document.createElement("div"));
+        this.element.id = "editor";
+
+        // Add some components
+        this.addComponent("panel", new EditorPanel);
+        this.addComponent("view", new EditorView);
+    }
+}
+
 // Export
-export default EditorPanel
-export { EditorMenu, EditorControls }
+export default Editor
+export {
+    Editor,
+    EditorMenu, EditorControls, EditorPanel,
+    EditorSideBar, EditorContent, EditorView
+}
 
 
