d2018f60632841ae09d52ba667256dc74d812939
Supernova12888 <supernova12888@gmail.com>
2022-09-10 17:24:07 -0400

    Moved code to `App` class

--- src/client/app/js/index.js
+++ src/client/app/js/index.js
@@ -11,69 +11,77 @@
 import dev from "./dev.js"
 import quill from "./quill.js"
 import {
-    Editor, Button, TextField, HorizontalResizer
+    Editor, HorizontalResizer
 } from "./components.js"
 
 
-/* =================== */
-/* Load                */
-/* =================== */
-
-// Globals
-const quickSettings = new Proxy({
-    getCookie: () => {
-        return quill.cookies.get("quick-settings");
+// App (singleton)
+class App {
+    constructor() {
+        dev.class.singleton();
     }
-}, {});
 
-// Components
-const editor = new Editor();
-const editorPanelMenu = editor.getComponent("panel", "menu");
-const editorViewSidebar = editor.getComponent("view", "side-bar");
-const editorViewContent = editor.getComponent("view", "content");
-
-// Add the horizontal resizer
-editorViewSidebar.addComponent("horizontalResizer", new HorizontalResizer("right"));
-
-// Add listeners
-editorViewSidebar.getComponent("horizontalResizer").setMousedownListener();
-editorViewSidebar.getComponent("horizontalResizer").setMouseupListener();
-editorViewSidebar.getComponent("horizontalResizer")
-    // The extra +/-55 is for the activity bar
-    .setMousemoveListener(function(event) {
-        // Collapse the editor navigation completely
-        // if the cursor reaches a certain point
-        if (event.clientX < 100 + 55) {
-            this.element.style.width = this.element.style.minWidth = "0px";
-            editorViewContent.element.style.width = `${dev.getPageSize().width - 55}px`;
-            return;
-        } else {
-            this.element.style.width = this.element.style.minWidth = "";
-        }
-
-        // Update width
-        const nav = this.element;
-        nav.style.minWidth = "";
-        nav.style.width = `${event.clientX - 55}px`;
-        editorViewContent.element.style.width = `${dev.getPageSize().width - event.clientX}px`;
-    });
-
-// Loading milestone (3/4)
-quill.milestoneTrack.done("loading");
+    // Components
+    static editor = new Editor();
+    static editorPanelMenu = App.editor.getComponent("panel", "menu");
+    static editorViewSidebar = App.editor.getComponent("view", "side-bar");
+    static editorViewContent = App.editor.getComponent("view", "content");
 
 
-/* =================== */
-/* Display             */
-/* =================== */
+    /* =================== */
+    /* Load                */
+    /* =================== */
+    static load() {
+        // Add the horizontal resizer
+        App.editorViewSidebar.addComponent("horizontalResizer", new HorizontalResizer("right"));
+        
+        // Add listeners
+        App.editorViewSidebar.getComponent("horizontalResizer").setMousedownListener();
+        App.editorViewSidebar.getComponent("horizontalResizer").setMouseupListener();
+        App.editorViewSidebar.getComponent("horizontalResizer")
+            // The extra +/-55 is for the activity bar
+            .setMousemoveListener(function(event) {
+                // Collapse the editor navigation completely
+                // if the cursor reaches a certain point
+                if (event.clientX < 100 + 55) {
+                    this.element.style.width = this.element.style.minWidth = "0px";
+                    App.editorViewContent.element.style.width = `${dev.getPageSize().width - 55}px`;
+                    return;
+                } else {
+                    this.element.style.width = this.element.style.minWidth = "";
+                }
+        
+                // Update width
+                const nav = this.element;
+                nav.style.minWidth = "";
+                nav.style.width = `${event.clientX - 55}px`;
+                App.editorViewContent.element.style.width = `${dev.getPageSize().width - event.clientX}px`;
+            });
+        
+        // Loading milestone (3/4)
+        quill.milestoneTrack.done("loading");
+    }
 
-editor.render(quill.app);
 
-// Auto activate the Edit button
-// This must be done after the editor is rendered
-// Otherwise, the active indicator cannot use .getBoundingClientRect()
-editor.getComponent("panel").controlsNavigator.menu.components["button-1"].activate();
+    /* =================== */
+    /* Display             */
+    /* =================== */
+    static display() {
+        // Render
+        App.editor.render(quill.app);
+        
+        // Auto activate the Edit button
+        // This must be done after the editor is rendered
+        // Otherwise, the active indicator cannot use .getBoundingClientRect()
+        App.editor.getComponent("panel").controlsNavigator.menu.components["button-1"].activate();
 
-// Display milestone (4/4)
-quill.milestoneTrack.done("display");
+        // Display milestone (4/4)
+        quill.milestoneTrack.done("display");
+    }
+}
+
+App.load();
+
+App.display();
 
 
