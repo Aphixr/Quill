4515f664305de05a7a30a01737dec3facc8d6b01
Supernova12888 <supernova12888@gmail.com>
2022-10-12 09:37:16 -0400

    Add size params and fix opposite position in `Icon`

--- src/client/app/js/components/graphics.js
+++ src/client/app/js/components/graphics.js
@@ -12,29 +12,42 @@ import { Component } from "../quartz.js"
 
 // SpriteSheet class
 class SpriteSheet {
-    constructor(path, intervalWidth, intervalHeight) {
+    constructor(path, intervalWidth=0, intervalHeight=0) {
         this.path = path;
         this.intervalWidth = +intervalWidth;
         this.intervalHeight = +intervalHeight;
     }
-    createIcon(x, y) {
-        return new Icon(this.path, x * this.intervalWidth, y * this.intervalHeight);
+
+    // Returns an Icon
+    getIcon(x=0, y=0, width=this.intervalWidth, height=this.intervalHeight) {
+        return new Icon(this.path, x * this.intervalWidth, y * this.intervalHeight, width, height);
     }
 }
 
 // Icon class
 class Icon extends Component {
-    constructor(source, x, y) {
+    constructor(source, x, y, width, height) {
         super(document.createElement("span"));
         this.classes.add("icon");
 
-        // Set the background
+        // Set the background image to the source
         this.style.backgroundImage = `url('${source}')`;
+
+        // Set background position
+        // x and y needs to be negative
         if (x) {
-            this.style.backgroundPositionX = x + "px";
+            this.style.backgroundPositionX = -x + "px";
         }
         if (y) {
-            this.style.backgroundPositionY = y + "px";
+            this.style.backgroundPositionY = -y + "px";
+        }
+
+        // Change the size
+        if (width) {
+            this.style.width = width + "px";
+        }
+        if (height) {
+            this.style.height = height + "px";
         }
     }
 }
